ccaaf1ef9c377c28f42577f3cd01283f46d387e7
Abdul Ahad <abdulahad43149031@gmail.com>
2025-11-16 07:25:40 +0530

    Added matrix as pointers

--- matrixAndPointers.c
+++ matrixAndPointers.c
@@ -19,11 +19,34 @@ void displayMat(int r, int c, int mat[r][c]){ // matrix is call by reference
     }
 }
 
+void matrixAndPointers(){
+    // inputing and outputing a matrix using pointers
+
+    int r = 3;
+    int c = 3;
+    int mat[3][3];
+
+    int* ptr = &mat[0][0];
+    printf("Enter 9 elements:\n");
+    for(int i = 0; i < r * c; i++){
+        scanf("%d", (ptr + i));
+    }
+
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            printf("%d ", *(ptr + (i * c) + j));
+        }
+        printf("\n");
+    }
+}
+
 
 int main(){
-    int row = 2;
-    int column = 3;
-    int mat[row][column];
-    inputMatrix(row, column, mat);
-    displayMat(row, column, mat);
+    // int row = 2;
+    // int column = 3;
+    // int mat[row][column];
+    // inputMatrix(row, column, mat);
+    // displayMat(row, column, mat);
+
+    matrixAndPointers();
 }
